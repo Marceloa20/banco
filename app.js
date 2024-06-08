929361a8493fde4951ec2222db4d99e3225f46fa
MARCELO MOTTA AVINTE <MARCELO0079@DIGITALCOLLEGE.LOCAL>
2024-06-07 21:36:58 -0300

    Implementando da conta mockada

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 import express from 'express'
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/conta:/id', (req, res) => {
+
+  res.json({
+    conta_id: 1234,
+    valor: 10.00
+  })
+})
+
+app.post('/post', (req, res) => {
+  const { conta_id, saldo } = req.body
+  if(!conta_id || !saldo) {
+    res.status(400).json({message: "Os campos conta e saldo são obrigatórios."})
+  }
+
+  res.status(201).json({
+    conta_id: 1234,
+    valor: 10.00
+  })
 })
 
 app.listen(port, () => {
